778adbe1af4818e4e5df53bf1a83a7ad4cfd0b04
Gal Koaz <76553647+GalKoaz@users.noreply.github.com>
2023-09-09 20:04:30 +0300

    upload dashboard and restrict user

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import './Register.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from "react-router-dom";
 
 
 export default function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -38,6 +41,17 @@ export default function Register() {
         setPassword('');
         setConfirmPassword('');
         toast.success("Registration successful");
+        setTimeout(() => {
+          toast.success("Moving to the Login Page!", {
+            style: {
+              backgroundColor: 'black',
+              color: 'white',
+            },
+          });
+        }, 1000);
+        setTimeout(() => {
+          navigate('/login');
+        }, 5000);
       } else {
         console.error('Registration failed');
         setPassword('');
@@ -46,7 +60,7 @@ export default function Register() {
       }
     } catch (error) {
       console.error('Error:', error);
-      toast.error("Registration failed");
+      toast.error('Error: ' + error);
       setEmail('');
       setPassword('');
       setConfirmPassword('');
--- server/server.js
+++ server/server.js
@@ -75,6 +75,16 @@ app.post("/login", async (req, res) => {
         }
 });
 
+app.get("/logout", async (req, res) => {
+    try {
+        res.status(200).json({ message: "Logout successful" });
+        } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Logout failed" });
+        }
+});
+
+
   
 
 app.listen(port, () => {
