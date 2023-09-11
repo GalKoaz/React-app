5591eeb08555ba7b1b82c4a5367c2e905babab2b
Gal Koaz <76553647+GalKoaz@users.noreply.github.com>
2023-09-12 01:44:59 +0300

    upload item.css

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 import './Item.css'
 
 export default function Item(props) {
   return (
     <div className="todo-item">
     <li key={props.id} id={props.id}>{props.item}<DeleteIcon className="delete-icon" onClick={()=>{
-        props.onChecked(props.id);
-    }}/></li>
+        props.onChecked(props.id);}}/> <EditIcon className='edit-icon' onClick={()=>{
+            props.onChecked2(props.id);
+        }}/></li>
     </div>
   );
 }
